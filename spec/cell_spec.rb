e1a1c7569d29516e6cba12fd8a4114fa629b3c38
samira-sadat48 <samira.sadat48@gmail.com>
2021-10-01 13:02:10 -0600

    Wrote a test for cell initialization

--- spec/cell_spec.rb
+++ spec/cell_spec.rb
@@ -2,7 +2,19 @@
 require "spec_helper"
  
 module TicTacToe
-  describe Cell do
-    # tests will be added here
-  end
+    RSpec.describe Cell do
+   
+      context "#initialize" do
+        it "is initialized with a value of '' by default" do
+          cell = Cell.new
+          expect(cell.value).to eq ''
+        end
+
+        it "can be initialized with a value of 'X'" do
+            cell = Cell.new("X")
+            expect(cell.value).to eq "X"
+        end
+      end
+   
+    end
 end
