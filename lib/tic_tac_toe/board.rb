34159f4a09ce30e8d1bd27850207166253620baf
samira-sadat48 <samira.sadat48@gmail.com>
2021-10-01 14:10:41 -0600

    Added getter and setter for a cell within a grid

--- lib/tic_tac_toe/board.rb
+++ lib/tic_tac_toe/board.rb
@@ -11,5 +11,16 @@ module TicTacToe
             # 3x3 Grid of Empty Cells
             Array.new(3) { Array.new(3) { Cell.new } }
         end
+
+        public
+
+        def get_cell(x,y)
+            grid[y][x]
+        end
+
+        def set_cell(x, y, value)
+            get_cell(x, y).value = value
+        end   
+
     end
 end
