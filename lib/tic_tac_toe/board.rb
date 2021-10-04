7a757f584aa176985ece084e81ef4089730f39dc
samira-sadat48 <samira.sadat48@gmail.com>
2021-10-04 15:57:42 -0600

    Added new methods to array class. Finished methods to check for game over

--- lib/tic_tac_toe/board.rb
+++ lib/tic_tac_toe/board.rb
@@ -20,7 +20,41 @@ module TicTacToe
 
         def set_cell(x, y, value)
             get_cell(x, y).value = value
-        end   
+        end  
+        
+        def game_over
+            return :winner if winner?
+            return :draw if draw?
+            false
+        end
 
+        def draw?
+            grid.flatten.map { |cell| cell.value }.none_empty?
+        end
+
+        def winning_positions
+            grid + # rows
+            grid.transpose + # columns
+            diagonals # two diagonals
+        end
+           
+        def diagonals
+            [
+              [get_cell(0, 0), get_cell(1, 1), get_cell(2, 2)],
+              [get_cell(0, 2), get_cell(1, 1), get_cell(2, 0)]
+            ]
+        end
+
+        def winner?
+            winning_positions.each do |winning_position|
+              next if winning_position_values(winning_position).all_empty?
+              return true if winning_position_values(winning_position).all_same?
+            end
+            false
+        end
+           
+        def winning_position_values(winning_position)
+            winning_position.map { |cell| cell.value }
+        end
     end
 end
